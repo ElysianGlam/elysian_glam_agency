a8a48dfc1e7524d51f051b1276e3a82e5d89a102
Elysian Glam <elysian.glam.agency@gmail.com>
2024-12-18 22:24:01 +0700

    11

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { writeFileSync } from 'fs';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/elysian_glam_agency/' // Имя вашего репозитория
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  postBuild() {
+    writeFileSync('dist/CNAME', 'elysianglam.agency');
+  }
 });
